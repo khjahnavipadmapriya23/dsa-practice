739371d1b78e28c3f04b9760d7f1e67dca6effde
KH Jahnavi Padma Priya <khjahnavipadmapriya23@gmail.com>
2026-02-05 23:20:50 +0530

    Add RightTriangle pattern program

---
+++ patterns/RightTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class RightTriangle{
+    public void ptn(int n){
+        if(n<=0){
+	        System.out.println("Enter a positive number");
+	    return;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+	    System.out.println();
+        }
+    }
+	public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter a number:");
+        int n=sc.nextInt();
+        RightTriangle obj = new RightTriangle();
+        obj.ptn(n);
+        sc.close();
+    }
+}
